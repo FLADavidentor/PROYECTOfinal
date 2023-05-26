b836c2cbc2c6822c460c991376b4e505577d6a92
Rodri2121 <130482477+Rodri2121@users.noreply.github.com>
2023-05-26 02:12:57 -0600

    Avances

--- Programa final/Inventariocel.cpp
+++ Programa final/Inventariocel.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <ctime>
 #include <sstream>
-#include "auxiliar.h"
+#include "auxiliarCyber.h"
 #define MAX 150
 #define CS 36
 #define MAX_ENTSAL 500
@@ -23,7 +23,7 @@ string FechaActual()
 
 struct Inventario
 {
-    char codproductoinv[3];
+    int codproductoinv;
     char producto[20];
     float precio;
     int existencias;
@@ -45,11 +45,10 @@ int TamEntsal = 0;
 
 struct CredencialesA
 {
-    char nickname[CS];
-    char password[CS];
+    char nickname[CS] = "Admin";
+    char password[CS] = "1234";
 };
-CredencialesA Credenciales[10];
-int TamCred = 0;
+CredencialesA Credenciales;
 
 /*
 Create
@@ -60,16 +59,14 @@ Delete
 
 /*Create*/
 void agregarProducto(Inventario Inv);
-void agregarCredenciales(CredencialesA Cred);
 /*Read*/
 void mostrarProducto(int pos, int valid);
 void mostrarProductos(int valid);                   // para mostrar todos los productos hacer un if con un bucle dentro y en el otro if solo imprimir una vez (el bool)
 int buscarEntsal(char codproducto[], char fecha[]); // Producto o fecha
-int buscarProducto(char codproducto[]);
+int buscarProducto(int codproducto[]);
 void iniciarProducto(int pos);
-void mostrarCredenciales(int posC);
-int buscarCredenciales();
 Inventario obtenerInv(int pos);
+int buscarCredenciales();
 /*Update*/
 void editarProducto(Inventario Inv, int pos);
 /*Delete*/
@@ -86,10 +83,6 @@ FILE *Inventariofile;
 void guardarInv();
 void leerInv();
 int CalcTamInv(FILE *archivo);
-FILE *Credencialesfile;
-void guardarCred();
-void leerCred();
-int CalcTamCred(FILE *archivo);
 
 void agregarProducto(Inventario Inv)
 {
@@ -138,7 +131,7 @@ void mostrarProducto(int pos, int valid)
         cout << "Existencias: " << Items[pos].existencias << endl;
         gotoxy(10, 8);
         cout << "Descripcion: \n"
-             << Items[pos].descripcion << endl;
+             << Items[pos].descripcion << "\n============================================="<<endl;
     }
 }
 
@@ -147,12 +140,12 @@ Inventario obtenerInv(int pos)
     return Items[pos];
 }
 
-int buscarProducto(char codproducto[])
+int buscarProducto(int codproducto)
 {
     int posicion = 0;
     for (int i = 0; i < TamInv; i++)
     {
-        if (strcmp(codproducto, Items[i].codproductoinv) == 0)
+        if (codproducto==Items[i].codproductoinv)
         {
             posicion = i;
             break;
@@ -176,55 +169,65 @@ void mostrarProductos(int valid = 0)
     }
 }
 
+void eliminarProducto(int pos)
+{
+    if (pos == TamInv)
+    {
+        cout << "No hay Producto\n";
+        return;
+    }
+    for (int i = pos; i < TamInv - 1; i++)
+    {
+        Items[i] = Items[i + 1];
+    }
+    TamInv--;
+    iniciarProducto(TamInv);
+}
+
+void iniciarProducto(int pos)
+{
+    Items[pos].codproductoinv=0;
+    strcpy(Items[pos].producto, "");
+    Items[pos].precio = 0;
+    strcpy(Items[pos].descripcion, "");
+    Items[pos].existencias = 0;
+}
 void editarProducto(Inventario Inv, int pos)
 {
     Items[pos] = Inv;
 }
 
-void Logo()
-{
-    cout << "\033[1;33m"
-         << "CyberStock"
-         << "\033[0m" << endl;
-}
-
 int buscarCredenciales()
 {
-    leerCred();
     bool Pase = false;
     char nick[CS];
     char passw[CS];
     CredencialesA Cred;
-    gotoxy(40, 4);
-    Logo();
-    gotoxy(40, 6);
-
+    gotoxy(40, 2);
+    cout << "\033[1;33m"
+         << "CyberStock"
+         << "\033[0m";
+    gotoxy(40, 5);
     cout << "Ingrese sus credenciales";
-
     gotoxy(35, 7);
-
     cout << "Nombre: ";
-
-    gotoxy(35, 8);
-    cout << "Contraseña: ";
-
-    gotoxy(44, 7);
-
+    gotoxy(45, 7);
     scanf(" %[^\n]", nick);
-
-    gotoxy(47, 8);
-
+    system("cls");
+    gotoxy(40, 2);
+    cout << "\033[1;33m"
+         << "CyberStock"
+         << "\033[0m";
+    gotoxy(40, 5);
+    cout << "Ingrese sus credenciales";
+    gotoxy(35, 7);
+    cout << "Contrasenia: ";
+    gotoxy(49, 7);
     scanf(" %[^\n]", passw);
 
     if (strcmp(Cred.nickname, nick) == 0 && strcmp(Cred.password, passw) == 0)
     {
         Pase = true;
-    }
-    else
-    {
-        system("cls");
-        gotoxy(40, 5);
-        cout << "CONTRASENIA O USUARIO NO VALIDO";
     }
     return Pase;
 }
@@ -232,123 +235,155 @@ int buscarCredenciales()
 //================================================MENUS============================================
 int menu()
 {
+    system("cls");
     int op;
-    int Pase;
-    Pase = buscarCredenciales();
-    if (Pase == true)
-    {
-                gotoxy(25, 5);
-            Logo();
-        gotoxy(15, 6);
-        cout << "Cantidad de registros: " << TamInv << endl;
-        gotoxy(10, 9);
-        cout << " 1. Agregar Producto \n";
-        gotoxy(10, 10);
-        cout << " 2. Editar Producto \n";
-        gotoxy(10, 11);
-        cout << " 3. Eliminar Producto \n";
-        gotoxy(10, 12);
-        cout << " 4. Buscar Producto \n";
-        gotoxy(10, 13);
-        cout << " 5. Mostrar Inventario \n";
-        gotoxy(10, 14);
-        cout << " 6. Mostrar Entradas y Salidas \n";
-        gotoxy(10, 15);
-        cout << " 7. Buscar Entradas y Salidas \n";
-        gotoxy(10, 16);
-        cout << " 8. Mostrar Credenciales \n";
-        gotoxy(10, 17);
-        cout << " 9. Agregar Credenciales \n";
-        gotoxy(10, 18);
-        cout << " 10. Editar Credenciales \n";
-        gotoxy(10, 19);
-        cout << " 11. Eliminar Credenciales \n";
-        gotoxy(10, 20);
-        cout << " 12. Administrar Existencias \n";
-        gotoxy(10, 21);
-        cout << " Digite la opcion: ";
-        gotoxy(29, 21);
-        cin >> op;
-    }
+    gotoxy(25, 5);
+    cout << "\033[1;33m"
+         << "CyberStock"
+         << "\033[0m" << endl;
+    gotoxy(15, 6);
+    cout << "Cantidad de registros: " << TamInv << endl;
+    gotoxy(10, 9);
+    cout << " 1. Agregar Producto \n";
+    gotoxy(10, 10);
+    cout << " 2. Editar Producto \n";
+    gotoxy(10, 11);
+    cout << " 3. Eliminar Producto \n";
+    gotoxy(10, 12);
+    cout << " 4. Buscar Producto \n";
+    gotoxy(10, 13);
+    cout << " 5. Mostrar Inventario \n";
+    gotoxy(10, 14);
+    cout << " 6. Mostrar Entradas y Salidas \n";
+    gotoxy(10, 15);
+    cout << " 7. Buscar Entradas y Salidas \n";
+    gotoxy(10, 16);
+    cout << " 8. Administrar Existencias \n";
+    gotoxy(10, 17);
+    cout << " Digite la opcion: ";
+    gotoxy(29, 18);
+    cin >> op;
+
     return op;
 }
 
 void start()
 {
+    int valid=0;
     int op;
-    char codproduct[3];
+    int codproduct;
     int pos;
+    bool Pase;
     Inventario Inv;
-    op=menu();
-    do
+    CredencialesA Cred;
+    leerInv();
+    Pase = buscarCredenciales();
+    if (Pase == true)
     {
-        switch (op)
+        do
         {
-        case 1:
-            gotoxy(30, 4);
-            cout << "Codigo de producto: ";
-            gotoxy(30, 5);
-            cout << "Producto: ";
-            gotoxy(30, 6);
-            cout << "Precio: $";
-            gotoxy(30, 7);
-            cout << "Existencias: ";
-            gotoxy(10, 8);
-            cout << "Descripcion: ";
-            gotoxy(50, 4);
-            cin >> Inv.codproductoinv;
-            gotoxy(40, 5);
-            scanf(" %[^\n]", Inv.producto);
-            gotoxy(39, 6);
-            cin >> Inv.precio;
-            gotoxy(43, 7);
-            cin >> Inv.existencias;
-            gotoxy(23, 8);
-            scanf(" %[^\n]", Inv.descripcion);
-            agregarProducto(Inv);
-            system("pause");
-            guardarInv();
-            break;
-        case 2:
-            system("cls");
-            cout << "Escribe el Codigo del producto a buscar: ";
-            scanf(" %[^\n]", codproduct);
-            system("cls");
-            pos = buscarProducto(codproduct);
-            gotoxy(6, 3);
-            cout << "DATOS ACTUALES\n";
-            mostrarProducto(pos, 0);
-            gotoxy(50, 3);
-            cout << "DATOS A EDITAR\n";
-            gotoxy(40, 4);
-            cout << "Codigo de producto: ";
-            gotoxy(40, 5);
-            cout << "Producto: ";
-            gotoxy(40, 6);
-            cout << "Precio: $";
-            gotoxy(40, 7);
-            cout << "Existencias: ";
-            gotoxy(40, 8);
-            cout << "Descripcion: ";
-            gotoxy(60, 4);
-            scanf(" %[^\n]", Inv.codproductoinv);
-            gotoxy(48, 5);
-            scanf(" %[^\n]", Inv.producto);
-            gotoxy(50, 6);
-            cin >> Inv.precio;
-            gotoxy(46, 7);
-            cin >> Inv.existencias;
-            gotoxy(47, 8);
-            scanf(" %[^\n]", Inv.descripcion);
-            editarProducto(Inv, pos);
-            gotoxy(40, 11);
-            cout << "Registro actualizado...\n";
-            system("pause");
-            guardarInv();
-        default:
-            break;
-        }
-    } while (op != 13);
+            op = menu();
+            switch (op)
+            {
+            case 1:
+                system("cls");
+                gotoxy(30, 4);
+                cout << "Codigo de producto: ";
+                gotoxy(30, 5);
+                cout << "Producto: ";
+                gotoxy(30, 6);
+                cout << "Precio: $";
+                gotoxy(30, 7);
+                cout << "Existencias: ";
+                gotoxy(10, 8);
+                cout << "Descripcion: ";
+                gotoxy(50, 4);
+                cin >> Inv.codproductoinv;
+                gotoxy(40, 5);
+                scanf(" %[^\n]", Inv.producto);
+                gotoxy(39, 6);
+                cin >> Inv.precio;
+                gotoxy(43, 7);
+                cin >> Inv.existencias;
+                gotoxy(23, 8);
+                scanf(" %[^\n]", Inv.descripcion);
+                agregarProducto(Inv);
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                cout << "Escribe el Codigo del producto a buscar: ";
+                cin >> codproduct;
+                system("cls");
+                pos = buscarProducto(codproduct);
+                gotoxy(6, 3);
+                cout << "DATOS ACTUALES\n";
+                mostrarProducto(pos, 0);
+                gotoxy(50, 3);
+                cout << "DATOS A EDITAR\n";
+                gotoxy(40, 4);
+                cout << "Codigo de producto: ";
+                gotoxy(40, 5);
+                cout << "Producto: ";
+                gotoxy(40, 6);
+                cout << "Precio: $";
+                gotoxy(40, 7);
+                cout << "Existencias: ";
+                gotoxy(40, 8);
+                cout << "Descripcion: ";
+                gotoxy(60, 4);
+                cin>> Inv.codproductoinv;
+                gotoxy(48, 5);
+                scanf(" %[^\n]", Inv.producto);
+                gotoxy(50, 6);
+                cin >> Inv.precio;
+                gotoxy(46, 7);
+                cin >> Inv.existencias;
+                gotoxy(47, 8);
+                scanf(" %[^\n]", Inv.descripcion);
+                editarProducto(Inv, pos);
+                gotoxy(40, 11);
+                cout << "Registro actualizado...\n";
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                cout << "Escribe el Codigo del producto a Eliminar: ";
+                cin>> codproduct;
+                system("cls");
+                pos = buscarProducto(codproduct);
+                gotoxy(6, 3);
+                cout << "DATOS ACTUALES\n";
+                mostrarProducto(pos, 0);
+                cout << "Registro actualizado...\n";
+                system("pause");
+            case 4:
+                system("cls");
+                cout << "Escribe el Codigo del producto a buscar: ";
+                cin>> codproduct;
+                system("cls");
+                pos = buscarProducto(codproduct);
+                gotoxy(6, 3);
+                cout << "PRODUCTO: \n";
+                mostrarProducto(pos, valid);
+                system("pause");
+                break;
+            case 5:
+                mostrarProductos();
+                system("pause");
+                break;
+            default:
+                break;
+            }
+        } while (op != 9);
+    }
+    else
+    {
+        system("cls");
+        gotoxy(40, 5);
+        cout << "CONTRASENIA O USUARIO NO VALIDO";
+    }
+    guardarInv();
 }
 
 // Para calcular el numero de productos totales guardados y guardar archivo bin
@@ -384,37 +419,4 @@ int CalcTamInv(FILE *archivo)
     // Calcular el número de productos
     num_productos = tam_archivo / sizeof(Inventario); // Divide el tamaño total del archivo por el tamaño de la estructura de inventario para obtener el número de productos almacenados en el archivo
     return num_productos;                             // Devuelve el número de productos almacenados en el archivo
-}
-
-void guardarCred()
-{
-    Credencialesfile = fopen("datosCred.bin", "wb");                        // Abre el archivo "datos.bin" en modo de escritura binaria
-    fwrite(Credenciales, sizeof(CredencialesA), TamCred, Credencialesfile); // Escribe los datos del inventario en el archivo
-    fclose(Credencialesfile);                                               // Cierra el archivo
-}
-
-void leerCred()
-{
-    Credencialesfile = fopen("datosCred.bin", "rb"); // Abre el archivo "datos.bin" en modo de lectura binaria
-    if (Credencialesfile == NULL)                    // Comprueba si el archivo no existe
-    {
-        return; // Si el archivo no existe, la función simplemente regresa
-    }
-    TamCred = CalcTamCred(Credencialesfile);                          // Calcula el número de productos en el archivo y lo almacena en la variable TamInv
-    fread(Credenciales, sizeof(CredencialesA), 10, Credencialesfile); // Lee los datos del archivo y los guarda en la estructura de inventario Items
-    fclose(Credencialesfile);                                         // Cierra el archivo
-}
-
-int CalcTamCred(FILE *archivo)
-{
-    int tam_archivo, num_Credenciales; // Declaración de variables locales
-
-    // Obtener el tamaño delarchivo
-    fseek(archivo, 0, SEEK_END);  // Mueve el puntero del archivo al final del archivo
-    tam_archivo = ftell(archivo); // Obtiene la posición actual del puntero, que corresponde al tamaño total del archivo
-    rewind(archivo);              // Mueve el puntero del archivo al inicio del archivo
-
-    // Calcular el número de productos
-    num_Credenciales = tam_archivo / sizeof(CredencialesA); // Divide el tamaño total del archivo por el tamaño de la estructura de inventario para obtener el número de productos almacenados en el archivo
-    return num_Credenciales;                                // Devuelve el número de productos almacenados en el archivo
 }
